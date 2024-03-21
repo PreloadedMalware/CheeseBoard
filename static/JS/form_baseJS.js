4854aeaa3c8cc59dd603c6d9564efe297bba3587
PreloadedMalware <hdu3bp@gmail.com>
2024-03-21 19:52:55 +0000

    Moved JS to external files

---
+++ static/JS/form_baseJS.js
@@ -0,0 +1,6 @@
+function displayError(){
+    try {
+        alert(document.getElementsByClassName("errorlist nonfield").item(0).childNodes.item(0).firstChild.nodeValue);
+    } catch (e) {
+    }
+}
